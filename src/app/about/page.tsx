7c03a0a47378aeed782b8dd4e44a1be35ca64dae
tfkhdyt <tfkhdyt@proton.me>
2023-05-22 13:45:34 +0700

    feat(about): add about me section

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutMeSection from '@/components/About/AboutMeSection';
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <>
+      <AboutMeSection />
+    </>
+  );
+};
+
+export default AboutPage;
