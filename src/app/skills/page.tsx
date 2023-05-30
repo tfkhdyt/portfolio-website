00047f2c0608b6ee2ce6897582e2887ffe891668
tfkhdyt <tfkhdyt@proton.me>
2023-05-30 17:49:01 +0700

    feat(skills): add more data to library category

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -93,6 +93,71 @@ const SkillsPage = () => {
       category: 'Framework',
       photoUrl: 'wwebjs.png',
     },
+    {
+      name: 'Cobra',
+      category: 'Framework',
+      photoUrl: 'cobra.png',
+    },
+    {
+      name: 'React',
+      category: 'Library',
+      photoUrl: 'react.svg',
+    },
+    {
+      name: 'Prisma',
+      category: 'Library',
+      photoUrl: 'prisma.png',
+    },
+    {
+      name: 'tRPC',
+      category: 'Library',
+      photoUrl: 'trpc.svg',
+    },
+    {
+      name: 'Alpine.js',
+      category: 'Library',
+      photoUrl: 'alpine.svg',
+    },
+    {
+      name: 'Tailwind CSS',
+      category: 'Library',
+      photoUrl: 'tailwind.svg',
+    },
+    {
+      name: 'UnoCSS',
+      category: 'Library',
+      photoUrl: 'unocss.svg',
+    },
+    {
+      name: 'Mantine',
+      category: 'Library',
+      photoUrl: 'mantine.svg',
+    },
+    {
+      name: 'Bootstrap',
+      category: 'Library',
+      photoUrl: 'bootstrap.svg',
+    },
+    {
+      name: 'Jotai',
+      category: 'Library',
+      photoUrl: 'jotai.png',
+    },
+    {
+      name: 'Auth.js',
+      category: 'Library',
+      photoUrl: 'nextauth.png',
+    },
+    {
+      name: 'React Query',
+      category: 'Library',
+      photoUrl: 'react-query.svg',
+    },
+    {
+      name: 'Zod',
+      category: 'Library',
+      photoUrl: 'zod.svg',
+    },
   ];
   const [currentCategory, setCurrentCategory] = useState<(typeof skillCategories)[number]>('Language');
 
