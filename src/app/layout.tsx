537b817e82c240ec500332d8880d845e6acb016a
tfkhdyt <tfkhdyt@proton.me>
2023-06-02 14:55:52 +0700

    feat(layout): add scroll to top button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import Navbar from '@/components/Navbar';
+import ScrollToTopButton from '@/components/ScrollToTopButton';
 
 import { Metadata } from 'next';
 import localFont from 'next/font/local';
@@ -35,8 +36,9 @@ export default function RootLayout({
     <html lang='en' id='html' className={rubik.className}>
       <body className='bg-light-bg-primary text-light-fg-primary dark:bg-dark-bg-primary dark:text-dark-fg-primary'>
         <Navbar />
-        <div className='py-6 px-8 mx-auto mt-16 md:px-16 md:mt-14 lg:container lg:px-32 xl:px-72'>
+        <div className='relative py-6 px-8 mx-auto mt-16 md:px-16 md:mt-14 lg:container lg:px-32 xl:px-72'>
           {children}
+          <ScrollToTopButton />
         </div>
       </body>
     </html>
---
+++ src/components/ScrollToTopButton.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useWindowScroll } from '@uidotdev/usehooks';
+import clsx from 'clsx';
+
+const ScrollToTopButton = () => {
+  const [{ y }, scrollTo] = useWindowScroll();
+
+  return (
+    <button
+      onClick={() => scrollTo({ top: 0, behavior: 'smooth' })}
+      className={clsx(
+        'fixed right-5 bottom-5 p-2 rounded-full border border-gray-200 dark:border-gray-700 hover:bg-gray-200 bg-light-bg-secondary dark:bg-dark-bg-secondary dark:hover:bg-gray-700',
+        y > 100
+          ? 'block'
+          : 'opacity-0 pointer-events-none',
+      )}
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6'
+      >
+        <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18' />
+      </svg>
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
