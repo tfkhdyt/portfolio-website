feba82538fc46a4c9e58777159cd7f8337df8fd5
tfkhdyt <tfkhdyt@proton.me>
2023-06-03 10:14:40 +0700

    fix(scroll-to-top): reduce icon size

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -24,7 +24,7 @@ const ScrollToTopButton = () => {
         viewBox='0 0 24 24'
         strokeWidth={1.5}
         stroke='currentColor'
-        className='w-6 h-6'
+        className='w-4 h-4'
       >
         <path
           strokeLinecap='round'
