1246f92e8f5417b69f315da6fd437e7f5683fcbc
tfkhdyt <tfkhdyt@proton.me>
2023-06-03 11:31:12 +0700

    fix(navbar, tabs): fix underline color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -81,9 +81,10 @@ const Navbar = () => {
               <Link href={content.target} onClick={() => setOpen(false)}>
                 <button
                   className={clsx(
-                    'hover:text-blue-100 hover:border-blue-100 dark:hover:text-blue-100 dark:hover:border-blue-100 py-4 w-full text-left border-light-fg-secondary/50 dark:border-dark-fg-secondary/50 border-b',
+                    'hover:text-blue-100 hover:border-blue-100 dark:hover:text-blue-100 dark:hover:border-blue-100 py-4 w-full text-left',
                     pathname == content.target
-                      && 'text-green-200 border-green-200 dark:text-green-100 dark:border-green-100 border-b-2',
+                      ? 'text-green-200 border-green-200 dark:text-green-100 dark:border-green-100 border-b-2'
+                      : 'border-light-fg-secondary/50 dark:border-dark-fg-secondary/50 border-b',
                   )}
                 >
                   {content.title}
