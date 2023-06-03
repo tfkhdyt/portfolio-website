bf5fc13ebba0bc9a2dbe397ba2078e76a62c6e8a
tfkhdyt <tfkhdyt@proton.me>
2023-06-03 11:01:39 +0700

    feat: add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,86 @@
+import Title from '@/components/Title';
+
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Contact - Taufik Hidayat',
+};
+
+type SocialMedia = {
+  name: string;
+  username: string;
+  url: string;
+};
+
+const contents: SocialMedia[] = [
+  {
+    name: 'GitHub',
+    username: 'tfkhdyt',
+    url: 'https://github.com/tfkhdyt',
+  },
+  {
+    name: 'Telegram',
+    username: '@tfkhdyt',
+    url: 'https://t.me/tfkhdyt',
+  },
+  {
+    name: 'Mastodon',
+    username: '@tfkhdyt',
+    url: 'https://fosstodon.org/@tfkhdyt',
+  },
+  {
+    name: 'YouTube',
+    username: 'Taufik Hidayat',
+    url: 'https://youtube.com/tfkhdyt',
+  },
+  {
+    name: 'LinkedIn',
+    username: 'Taufik Hidayat',
+    url: 'https://www.linkedin.com/in/tfkhdyt142/',
+  },
+  {
+    name: 'Facebook',
+    username: 'Taufik Hidayat',
+    url: 'https://www.facebook.com/tfkhdyt142/',
+  },
+  {
+    name: 'Email',
+    username: 'tfkhdyt@proton.me',
+    url: 'mailto:tfkhdyt@proton.me',
+  },
+];
+
+const ContactPage = () => {
+  return (
+    <main className='space-y-4'>
+      <Title>Contact</Title>
+      <p className='dark:text-gray-200 text-light-fg-secondary'>
+        Please don’t hesitate to get in touch with me by following my social media below:
+      </p>
+      <div className='ml-8 text-base font-normal dark:text-gray-200 text-light-fg-secondary'>
+        <ul className='space-y-1 list-disc list-outside'>
+          {contents.map((content, idx) => (
+            <li key={idx}>
+              <span className='font-medium text-gray-800 dark:text-gray-400'>
+                {content.name}
+              </span>{' '}
+              -{' '}
+              <Link
+                href={content.url}
+                target='_blank'
+                className='relative font-bold underline group underline-offset-4 decoration-dashed decoration-1'
+              >
+                {content.username}
+                <span className='absolute left-0 -bottom-0.5 w-0 h-full border-b-2 group-hover:w-full'>
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default ContactPage;
