3a0ebb798f8c6f6f28ad98d7545ff9f90b46dd26
tfkhdyt <tfkhdyt@proton.me>
2023-06-03 17:46:02 +0700

    feat(projects): add more projects

--- src/components/Projects/ProjectCards.tsx
+++ src/components/Projects/ProjectCards.tsx
@@ -58,6 +58,86 @@ const projects: Project[] = [
     },
     photoUrl: 'pondokeun.png',
   },
+  {
+    name: 'WhatsApp Link Generator',
+    desc: 'Web app to generate WhatsApp contact link with default message easily',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'SvelteKit',
+      'Tailwind CSS',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/whatsapp-link-generator',
+      demo: 'https://walinkgen.tfkhdyt.my.id/',
+    },
+    photoUrl: 'whatsapp-link-generator.png',
+  },
+  {
+    name: 'M/M/s Queue Theory Calculator',
+    desc: 'Calculator to solve M/M/s queue theory problem, written in TypeScript and SvelteKit.',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'SvelteKit',
+      'Tailwind CSS',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/mms-queue-theory-calculator',
+      demo: 'https://queue.tfkhdyt.my.id/',
+    },
+    photoUrl: 'mms-calculator.png',
+  },
+  {
+    name: 'Portfolio Website v3',
+    desc: 'Third revision of my portfolio website',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'Next.js',
+      'Node.js',
+      'Mantine',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/web-portfolio-v3',
+      demo: 'https://www.tfkhdyt.my.id/',
+    },
+    photoUrl: 'portfolio-website-v3.png',
+  },
+  {
+    name: 'To Do List',
+    desc: 'Simple Todo List App with Authentication',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'Next.js',
+      'Node.js',
+      'Prisma',
+      'Mantine',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/todo-prisma',
+      demo: 'http://tfkhdyt-todo-list.vercel.app/',
+    },
+    photoUrl: 'todolist.png',
+  },
+  {
+    name: 'Full Stack Roadmap',
+    desc: 'Roadmap about how to become a Full Stack Developer',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'Next.js',
+      'NestJS',
+      'Node.js',
+      'MongoDB',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/full-stack-roadmap',
+      demo: 'https://roadmap.tfkhdyt.my.id/',
+    },
+    photoUrl: 'full-stack-roadmap.png',
+  },
 ];
 
 const ProjectCards = () => {
@@ -92,7 +172,7 @@ const ProjectCards = () => {
                 />
               </div>
               <div className='p-4 space-y-2'>
-                <div className='flex justify-between items-center'>
+                <div className='flex justify-between items-start'>
                   <p className='text-xl font-semibold'>{project.name}</p>
                   <div className='flex space-x-1'>
                     {project.url?.repo && (
