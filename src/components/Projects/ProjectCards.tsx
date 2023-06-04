ed5d46b091b7263d2d3cfe33240572f2286a8484
tfkhdyt <tfkhdyt@proton.me>
2023-06-04 10:37:49 +0700

    feat(projects): add more data

--- src/components/Projects/ProjectCards.tsx
+++ src/components/Projects/ProjectCards.tsx
@@ -138,6 +138,97 @@ const projects: Project[] = [
     },
     photoUrl: 'full-stack-roadmap.png',
   },
+  {
+    name: 'Portfolio Website v2',
+    desc: 'Second revision of my portfolio website',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'Next.js',
+      'Tailwind CSS',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/web-portfolio',
+      demo: 'https://v2.tfkhdyt.my.id',
+    },
+    photoUrl: 'portfolio-website-v2.png',
+  },
+  {
+    name: 'Bookstore',
+    desc: 'Books management app',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'Next.js',
+      'Golang',
+      'Gin Gonic',
+      'Mantine',
+      'PostgreSQL',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/bookstore',
+      demo: 'https://bookstore.tfkhdyt.my.id/',
+    },
+    photoUrl: 'bookstore.jpg',
+  },
+  {
+    name: 'LAM Final Project',
+    desc: 'Web app to solve some linear algebra and matrices problems',
+    category: 'Web',
+    techStack: [
+      'JavaScript',
+      'Next.js',
+      'Tailwind CSS',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/tugas-akhir-aljabar-linier-matriks',
+      demo: 'https://aljabar-linier-matriks-kelompok-2.vercel.app/',
+    },
+    photoUrl: 'lam-final-project.png',
+  },
+  {
+    name: 'Random Quote Generator',
+    desc: 'Web app that will generate a random quote for you',
+    category: 'Web',
+    techStack: [
+      'TypeScript',
+      'Next.js',
+      'Tailwind CSS',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/random-quote-generator',
+      demo: 'https://quotes.tfkhdyt.my.id/',
+    },
+    photoUrl: 'random-quote-generator.png',
+  },
+  {
+    name: 'Portfolio Website v1',
+    desc: 'My first portfolio website',
+    category: 'Web',
+    techStack: [
+      'JavaScript',
+      'Bootstrap',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/tfkhdyt-portfolio',
+      demo: 'https://tfkhdyt.web.app/',
+    },
+    photoUrl: 'portfolio-website-v1.png',
+  },
+  {
+    name: 'MovieDB',
+    desc: 'Web app to search movie information',
+    category: 'Web',
+    techStack: [
+      'JavaScript',
+      'Bootstrap',
+    ],
+    url: {
+      repo: 'https://github.com/tfkhdyt/imdb',
+      demo: 'https://movie.tfkhdyt.my.id/',
+    },
+    photoUrl: 'movie-db.png',
+  },
 ];
 
 const ProjectCards = () => {
@@ -171,69 +262,71 @@ const ProjectCards = () => {
                   loading={project.category === 'Web' ? 'eager' : 'lazy'}
                 />
               </div>
-              <div className='p-4 space-y-2'>
-                <div className='flex justify-between items-start'>
-                  <p className='text-xl font-semibold'>{project.name}</p>
-                  <div className='flex space-x-1'>
-                    {project.url?.repo && (
-                      <Link
-                        href={project.url.repo}
-                        target='_blank'
-                        aria-label={`${project.name}'s repository`}
-                      >
-                        <button
-                          className='p-1 rounded hover:bg-gray-300 dark:hover:bg-gray-800'
+              <div className='flex flex-col justify-between p-4 space-y-2 h-full'>
+                <div className='space-y-1'>
+                  <div className='flex justify-between items-start'>
+                    <p className='text-xl font-semibold'>{project.name}</p>
+                    <div className='flex space-x-1'>
+                      {project.url?.repo && (
+                        <Link
+                          href={project.url.repo}
+                          target='_blank'
                           aria-label={`${project.name}'s repository`}
                         >
-                          <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            strokeWidth={1.5}
-                            stroke='currentColor'
-                            className='w-6 h-6'
+                          <button
+                            className='p-1 rounded hover:bg-gray-300 dark:hover:bg-gray-800'
+                            aria-label={`${project.name}'s repository`}
                           >
-                            <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z'
-                            />
-                          </svg>
-                        </button>
-                      </Link>
-                    )}
-                    {project.url?.demo && (
-                      <Link
-                        href={project.url.demo}
-                        target='_blank'
-                        aria-label={`${project.name}'s demo`}
-                      >
-                        <button
-                          className='p-1 rounded hover:bg-gray-300 dark:hover:bg-gray-800'
+                            <svg
+                              xmlns='http://www.w3.org/2000/svg'
+                              fill='none'
+                              viewBox='0 0 24 24'
+                              strokeWidth={1.5}
+                              stroke='currentColor'
+                              className='w-6 h-6'
+                            >
+                              <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z'
+                              />
+                            </svg>
+                          </button>
+                        </Link>
+                      )}
+                      {project.url?.demo && (
+                        <Link
+                          href={project.url.demo}
+                          target='_blank'
                           aria-label={`${project.name}'s demo`}
                         >
-                          <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            strokeWidth={1.5}
-                            stroke='currentColor'
-                            className='w-6 h-6'
+                          <button
+                            className='p-1 rounded hover:bg-gray-300 dark:hover:bg-gray-800'
+                            aria-label={`${project.name}'s demo`}
                           >
-                            <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25'
-                            />
-                          </svg>
-                        </button>
-                      </Link>
-                    )}
+                            <svg
+                              xmlns='http://www.w3.org/2000/svg'
+                              fill='none'
+                              viewBox='0 0 24 24'
+                              strokeWidth={1.5}
+                              stroke='currentColor'
+                              className='w-6 h-6'
+                            >
+                              <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25'
+                              />
+                            </svg>
+                          </button>
+                        </Link>
+                      )}
+                    </div>
                   </div>
+                  <p className='dark:text-gray-200 text-light-fg-secondary'>
+                    {project.desc}
+                  </p>
                 </div>
-                <p className='dark:text-gray-200 text-light-fg-secondary'>
-                  {project.desc}
-                </p>
                 <div className='flex py-2 space-x-2'>
                   {skills
                     .filter((skill) => project.techStack.includes(skill.name))
