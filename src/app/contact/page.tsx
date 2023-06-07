4775db0d2b2c5f2ca7b9f64ee0b09e23538434e6
tfkhdyt <tfkhdyt@proton.me>
2023-06-07 09:43:37 +0700

    fix(contact): adjust underline offset

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -71,7 +71,7 @@ const ContactPage = () => {
                 <Link
                   href={content.url}
                   target='_blank'
-                  className='relative font-bold underline group underline-offset-4 decoration-dotted decoration-2'
+                  className='relative font-bold underline group underline-offset-[4.5px] decoration-dotted decoration-1 md:underline-offset-4 md:decoration-2'
                 >
                   {content.username}
                   <span className='absolute left-0 -bottom-0.5 w-0 h-full border-b-2 dark:border-gray-200 group-hover:w-full border-light-fg-secondary'>
