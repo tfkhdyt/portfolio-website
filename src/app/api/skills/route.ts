5858a96eaa67d8ab93ed817e63ed54e56887687e
tfkhdyt <tfkhdyt@proton.me>
2023-06-09 17:49:58 +0700

    feat(create-skill-api): add request body validation

--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -1,7 +1,23 @@
 import { skillService } from '@/skill/SkillService';
-import { HTTPError } from '@/utils/error';
+import { HTTPError, UnprocessableEntityError } from '@/utils/error';
 
 import { NextResponse } from 'next/server';
+import { z } from 'zod';
+
+const createSkillSchema = z.object({
+  name: z
+    .string({
+      required_error: 'Name is required',
+      invalid_type_error: 'Name should be in string',
+    })
+    .max(12, 'Name should not be more than 12 characters'),
+  categoryId: z
+    .string({
+      required_error: 'Category id is required',
+      invalid_type_error: 'Category id should be in string',
+    })
+    .cuid('Category id is invalid'),
+});
 
 export const POST = async (req: Request) => {
   try {
@@ -9,6 +25,12 @@ export const POST = async (req: Request) => {
 
     const name = formData.get('name') as string;
     const categoryId = formData.get('category') as string;
+
+    const { success } = createSkillSchema.safeParse({ name, categoryId });
+    if (!success) {
+      throw new UnprocessableEntityError('Request body is not valid');
+    }
+
     const photo = formData.get('photo') as File;
 
     const response = await skillService.createSkill({ name, categoryId, photo });
