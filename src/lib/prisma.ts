c1af142d4d417623102b2893d2c06bf5e1177984
tfkhdyt <tfkhdyt@proton.me>
2023-06-10 12:23:55 +0700

    refactor(auth): move authOptions to @/lib/prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,8 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import { AuthOptions } from 'next-auth';
+import { Adapter } from 'next-auth/adapters';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 let prisma: PrismaClient;
 
@@ -15,4 +19,36 @@ if (process.env.NODE_ENV === 'production') {
   prisma = global.prisma;
 }
 
-export { prisma };
+const ADMIN_EMAIL = process.env.ADMIN_EMAIL as string;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD as string;
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Admin Login',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (credentials?.email !== ADMIN_EMAIL) {
+          return null;
+        }
+
+        if (credentials.password !== ADMIN_PASSWORD) {
+          return null;
+        }
+
+        const user = { id: 'admin', name: 'admin', email: credentials.email };
+
+        return user;
+      },
+    }),
+  ],
+};
+
+export { authOptions, prisma };
