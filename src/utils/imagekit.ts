06ac020d2ce581a05a25e4b146008d789558e6ee
tfkhdyt <tfkhdyt@proton.me>
2023-06-13 19:21:26 +0700

    refactor(imagekit): move uploadImage and deleteImage method to utils

---
+++ src/utils/imagekit.ts
@@ -0,0 +1,36 @@
+import { imagekit } from '@/lib/imagekit';
+import { InternalServerError } from './error';
+
+export const uploadImage = async (image: File, folder: string) => {
+  try {
+    const { url, fileId } = await imagekit.upload({
+      file: Buffer.from(await image.arrayBuffer()),
+      fileName: image.name,
+      folder,
+    });
+
+    return { photoUrl: url, photoId: fileId };
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof Error) {
+      throw new InternalServerError(error.message);
+    }
+
+    throw new InternalServerError('Failed to upload image');
+  }
+};
+
+export const deleteImage = async (photoId: string) => {
+  try {
+    await imagekit.deleteFile(photoId);
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof Error) {
+      throw new InternalServerError(error.message);
+    }
+
+    throw new InternalServerError(`Failed to delete image with id ${photoId}`);
+  }
+};
