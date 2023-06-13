ffe4073765fca5872373299db49e3976fb224d91
tfkhdyt <tfkhdyt@proton.me>
2023-06-13 19:18:57 +0700

    fix(Avatar): disable blur placeholder

--- src/components/Home/Avatar.tsx
+++ src/components/Home/Avatar.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import avatar from '../../../public/img/tfkhdyt-avatar.svg';
-
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 
@@ -11,12 +9,11 @@ const Avatar = () => {
   return (
     <div>
       <Image
-        src={avatar}
+        src='/img/tfkhdyt-avatar.svg'
         alt='tfkhdyt avatar'
         className='w-64 h-64 md:h-auto'
         width={300}
         height={300}
-        placeholder='blur'
         priority
         onClick={() => session ? signOut() : signIn()}
       />
