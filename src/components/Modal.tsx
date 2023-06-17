104b2f1b0f34505a0b53c60912ba46d67d32e973
tfkhdyt <tfkhdyt@proton.me>
2023-06-17 07:41:06 +0700

    fix(modal): add props for trigger component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,16 +6,22 @@ type Props = {
   onOpenChange: (open: boolean) => void;
   title: string;
   children: ReactNode;
+  trigger: ReactNode;
+  triggerContainerClassNames?: string;
 };
 
-const Modal = ({ open, onOpenChange, title, children }: Props) => {
+const Modal = ({
+  open,
+  onOpenChange,
+  title,
+  children,
+  trigger,
+  triggerContainerClassNames,
+}: Props) => {
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
-      <Dialog.Trigger className='p-3 w-full bg-gradient-to-b from-transparent to-blue-100 rounded-bl-lg hover:to-blue-200'>
-        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='mx-auto w-6 h-6'>
-          <path d='M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z' />
-          <path d='M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z' />
-        </svg>
+      <Dialog.Trigger className={triggerContainerClassNames}>
+        {trigger}
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 z-50 bg-black/75' />
