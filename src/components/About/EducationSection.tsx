7805868a61e696b2b1679272ad11d5a7026d8671
tfkhdyt <me@tfkhdyt.my.id>
2023-09-11 07:03:10 +0700

    feat(education): update GPA

--- src/components/About/EducationSection.tsx
+++ src/components/About/EducationSection.tsx
@@ -5,7 +5,7 @@ const contents = [
   {
     title: 'Universitas Bale Bandung',
     period: '2020 - Present',
-    majors: 'Computer Science (Current GPA: 3.64)',
+    majors: 'Computer Science (Current GPA: 3.66)',
   },
   {
     title: 'SMKN 7 Baleendah',
@@ -14,15 +14,15 @@ const contents = [
   },
 ];
 
-export type Education = typeof contents[number];
+export type Education = (typeof contents)[number];
 
 const EducationSection = () => {
   return (
-    <div className='flex gap-16'>
-      <div className='space-y-6'>
+    <div className="flex gap-16">
+      <div className="space-y-6">
         <Title>Education</Title>
-        <div className='mx-4'>
-          <ol className='relative border-l border-gray-200 dark:border-gray-700'>
+        <div className="mx-4">
+          <ol className="relative border-l border-gray-200 dark:border-gray-700">
             {contents.map((content, idx) => (
               <EducationListItem
                 content={content}
