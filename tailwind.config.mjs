f74316ad89f72ea6096d1e64945afcb75a1c0507
tfkhdyt <tfkhdyt@proton.me>
2024-02-24 21:13:15 +0700

    chore(tailwind): Added custom container padding
    Updated the container padding in the Tailwind configuration file to allow for more customization.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,19 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '5rem',
+        xl: '6rem',
+        '2xl': '7rem',
+      },
+    },
     extend: {
       colors: {
         trueGray: {
+          50: '#fafafa',
           100: '#f5f5f5',
         },
       },
