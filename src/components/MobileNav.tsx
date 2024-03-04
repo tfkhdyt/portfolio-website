6ef07629afeb26f93109f957e594774d2c1cfca9
tfkhdyt <tfkhdyt@proton.me>
2024-03-04 11:50:09 +0700

    refactor: move navigations to constant
    Moved navigation array to a constant file to make it easier to maintain and update.
    
    BREAKING CHANGE: Navigations are now imported from a constant file, so any changes to the navigation array must be made in the constant file.

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,6 +1,7 @@
 import { MenuIcon } from 'lucide-react';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 import { useState } from 'react';
+import { navigations } from '@/constants/sections';
 
 const MobileNav = () => {
   const [open, setOpen] = useState(false);
@@ -17,41 +18,16 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className='w-[250px] border-gray-950 bg-gray-900 pt-12'>
         <div className='flex flex-col space-y-4  text-xl font-medium'>
-          <a
-            href='#home'
-            className='decoration-teal-300 decoration-[3px] underline-offset-[6px] hover:underline'
-            onClick={() => setOpen(false)}
-          >
-            Home
-          </a>
-          <a
-            href='#about'
-            className='decoration-teal-300 decoration-[3px] underline-offset-[6px] hover:underline'
-            onClick={() => setOpen(false)}
-          >
-            About
-          </a>
-          <a
-            href='#skill'
-            className='decoration-teal-300 decoration-[3px] underline-offset-[6px] hover:underline'
-            onClick={() => setOpen(false)}
-          >
-            Skill Set
-          </a>
-          <a
-            href='#portfolio'
-            className='decoration-teal-300 decoration-[3px] underline-offset-[6px] hover:underline'
-            onClick={() => setOpen(false)}
-          >
-            Portfolio
-          </a>
-          <a
-            href='#certifications'
-            className='decoration-teal-300 decoration-[3px] underline-offset-[6px] hover:underline'
-            onClick={() => setOpen(false)}
-          >
-            Certifications
-          </a>
+          {navigations.map((nav) => (
+            <a
+              href={nav.to}
+              className='w-fit decoration-teal-300 decoration-[3px] underline-offset-[6px] hover:underline'
+              onClick={() => setOpen(false)}
+              key={nav.name}
+            >
+              {nav.name}
+            </a>
+          ))}
         </div>
       </SheetContent>
     </Sheet>
---
+++ src/constants/sections.ts
@@ -0,0 +1,22 @@
+export const navigations = [
+  {
+    name: 'Home',
+    to: '#home',
+  },
+  {
+    name: 'About',
+    to: '#about',
+  },
+  {
+    name: 'Skill Set',
+    to: '#skill',
+  },
+  {
+    name: 'Portfolio',
+    to: '#portfolio',
+  },
+  {
+    name: 'Certifications',
+    to: '#certifications',
+  },
+];
