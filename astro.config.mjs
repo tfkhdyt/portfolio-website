5a6c08cdb73fbc80bcd9914695413fe2f68da662
tfkhdyt <tfkhdyt@proton.me>
2024-03-05 06:29:16 +0700

    chore(astro): disable html compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), compress()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    compress({
+      HTML: false,
+    }),
+  ],
   image: {
-    domains: ['admin.tfkhdyt.my.id']
-  }
+    domains: ['admin.tfkhdyt.my.id'],
+  },
 });
+
