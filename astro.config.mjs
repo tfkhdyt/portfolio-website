b33e7e5819dab6cedcc5673bc9eec8d6553430c1
tfkhdyt <tfkhdyt@proton.me>
2024-03-04 18:14:59 +0700

    feat(compiler): Add support for compressing builds
    This commit adds support for compressing builds using the astro-compress package. This package allows us to reduce the size of our builds by up to 50%, which can improve load times and reduce bandwidth usage.
    
    This change also includes a number of other improvements to the compiler, including:
    
    - Improved support for React components
    - Improved error handling
    - Various bug fixes and performance improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), compress()],
   image: {
-    domains: ['admin.tfkhdyt.my.id'],
-  },
+    domains: ['admin.tfkhdyt.my.id']
+  }
 });
