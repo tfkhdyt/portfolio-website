a7e7df8e0916043cd96aa3238e89424a06cc2825
Taufik Hidayat <taufik@generativestudio.dev>
2025-05-03 18:24:37 +0700

    Format with prettier

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,12 +2,12 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-        interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
-        }
-    }
+		interface Platform {
+			env: Env;
+			cf: CfProperties;
+			ctx: ExecutionContext;
+		}
+	}
 }
 
 export {};
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
